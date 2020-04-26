252d0f566ec60503570617d085346228caf2136a
sanosyu <sanosyu@MBP.local>
2020-04-26 16:15:29 +0900

    Add SG filter

--- helper.R
+++ helper.R
@@ -1,12 +1,32 @@
-#
+#　Preprocessing functions for spectrum data
+
+library(signal)
 
 SNV <- function(x){
   # x: data, query align to row
-  
   Mean <- apply(x, 1, mean)
   SD <- apply(x, 1, sd)
-  res <- x
-  res <- sweep(res, 1, Mean, FUN = '-')
+  res <- sweep(x  , 1, Mean, FUN = '-')
   res <- sweep(res, 1, SD,   FUN = "/")
   return(res)
 }
+
+
+SG <- function(x, m, p, n){
+  # Savitzky-Gollay fileter
+  # x: data, query align to row
+  # m: rank for derivative, 0: smoothing, 1: 1st derivative, 2: 2nd derivative
+  # p: polynomial order for regression curve
+  # n: window size, odd integar
+  
+  res <- apply(x, 1, function(i){
+    sgolayfilt(i, m = m, p = p, n = n )
+    })
+  res <- t(res)
+  Edge <- (n-1)/2
+  Cut <- c(1:Edge, tail(1:ncol(res), n = Edge))
+  res <- res[,-Cut]
+  return(res)
+}
+
+
